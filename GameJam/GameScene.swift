d6831ecbb7364cc62c5ca62ea6180f98d0970d8e
Nathan Feldsien <nfeld9807@gmail.com>
2016-01-29 22:07:39 -0500

    Removed old tiled code

--- GameJam/GameScene.swift
+++ GameJam/GameScene.swift
@@ -10,12 +10,7 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        let block = BlockNode(texture: nil, color: UIColor.blueColor(), size: self.size)
-        
-        let position = CGPoint(x: self.size.width / 2, y: self.size.height / 2)
-        block.position = position
-        
-        addChild(block)
+
     }
    
     override func update(currentTime: CFTimeInterval) {
