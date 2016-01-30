ff8fba8d29724db230314ad932725524faa71d3b
Nathan Feldsien <nfeld9807@gmail.com>
2016-01-30 12:34:52 -0500

    Adds gravity and vector launching

--- GameJam/GameScene.swift
+++ GameJam/GameScene.swift
@@ -10,7 +10,21 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-
+        let cannonNodeA = CannonNode()
+        cannonNodeA.position = CGPoint(x: self.size.width / 2, y: 100)
+        addChild(cannonNodeA)
+        
+        let cannonNodeB = CannonNode()
+        cannonNodeB.position = CGPoint(x: self.size.width / 2, y: 200)
+        addChild(cannonNodeB)
+        
+        let cannonNodeC = CannonNode()
+        cannonNodeC.position = CGPoint(x: self.size.width / 2, y: 400)
+        addChild(cannonNodeC)
+        
+        let cannonNodeD = CannonNode()
+        cannonNodeD.position = CGPoint(x: self.size.width / 2, y: 600)
+        addChild(cannonNodeD)
     }
    
     override func update(currentTime: CFTimeInterval) {
